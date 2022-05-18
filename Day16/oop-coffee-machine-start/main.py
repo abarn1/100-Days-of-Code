03078b23b8829e635522512c29a6f64bb62a836c
Alex Barnes <35738417+abarn1@users.noreply.github.com>
2022-05-17 23:39:50 -0700

    Finished the first 3 requirements for the coffee machine code and removed the useless file I had initially created

--- Day16/oop-coffee-machine-start/main.py
+++ Day16/oop-coffee-machine-start/main.py
@@ -1,5 +1,15 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-order = input('What would you like? (espresso/latte/cappuccino):')
-print(order)
+
+# need to build a while loop to ask the next customer what they want
+machine_state = True
+menu_items = Menu()
+machine_resources = CoffeeMaker()
+
+while machine_state:
+    order = input(f'What would you like? {menu_items.get_items()}: ')
+    if order == 'off':
+        machine_state = False
+    elif order == 'report':
+        print(machine_resources.report())

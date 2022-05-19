7824ef7469090fddaf5c7b5aa5923bc651acf4de
Alex Barnes <35738417+abarn1@users.noreply.github.com>
2022-05-18 23:16:08 -0700

    The class which was created in the inital description of the OOP section on day 17 of 100 days of code

--- Day17/quiz-game-start/main.py
+++ Day17/quiz-game-start/main.py
@@ -1,7 +1,23 @@
+# creates the user class(object) from the initial code example representation
 class User:
-    def __init__(self):
-        print("new user being created...")
+    # this init creates the initial values for the items in the object
+    def __init__(self, user_id, username):
+        # including a print statement here in the init will print every time the object is created
+        # print("new user being created...")
+        # initializes the id for the user based on the user_id passed in for the code
+        self.id = user_id
+        # initializes the username for the user based on the username passed in when the object was created
+        self.username = username
+        self.followers = 0
+        self.following = 0
 
-user_1 = User()
-user_1.id = '001'
-print(user_1.id)
+    # creates a method
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+user_1 = User('001', 'angela')
+user_2 = User('002', 'bob')
+
+user_1.follow(user_2)

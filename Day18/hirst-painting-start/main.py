ea1f5f35ae7c10fa620573d88ea16f2058ab23fc
Alex Barnes <35738417+abarn1@users.noreply.github.com>
2022-05-28 10:22:17 -0700

    setting up day 18

--- Day18/hirst-painting-start/main.py
+++ Day18/hirst-painting-start/main.py
@@ -1,5 +1,5 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
+# This code will not work in repl.it as there is no access to the colorgram package here.###
+# We talk about this in the video tutorials##
 import colorgram
 
 rgb_colors = []
@@ -8,3 +8,7 @@ for color in colors:
     rgb_colors.append(color.rgb)
 
 print(rgb_colors)
+
+# find the most common color in the image
+most_common_color = max(set(rgb_colors), key=rgb_colors.count)
+print(most_common_color)

fa47f84bbdc8fcd9c081952a0458964a11749dd7
Alex Barnes <35738417+abarn1@users.noreply.github.com>
2022-05-29 18:25:47 -0700

    Finished day 19 challenges and problems

--- Day19/etch-a-sketch-start/main.py
+++ Day19/etch-a-sketch-start/main.py
@@ -6,8 +6,17 @@ screen = Screen()
 
 def move_forwards():
     tim.forward(10)
-
+def move_backwards():
+    tim.backward(10)
+def turn_left():
+    tim.left(5)
+def turn_right():
+    tim.right(5)
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=screen.resetscreen)
 screen.exitonclick()
--- Day19/turtle-race-start/main.py
+++ Day19/turtle-race-start/main.py
@@ -1,7 +1,41 @@
+from socketserver import StreamRequestHandler
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+
 screen = Screen()
+screen.setup(width=800, height=600)
+user_bet = screen.textinput(title="Bet input", prompt="Which color would you like to bet on?")
+is_race_on = False
+colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "black"]
+list_of_turtles = []
+num_of_turtles = 6
 
+
+for count in range(num_of_turtles):
+    turtle = Turtle()
+    turtle.color(colors[count])
+    turtle.penup()
+    turtle.goto(-380, (count - num_of_turtles/2) * 50)
+    turtle.shape("turtle")
+    turtle.pendown()
+    count += 1
+    list_of_turtles.append(turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for tut in list_of_turtles:
+        distance_to_move = random.randint(0,20)
+        tut.forward(distance_to_move)
+        if tut.xcor() > 380:
+            is_race_on = False
+            if tut.pencolor() == user_bet:
+                print("You chose the correct turtle!!")
+                break
+            else:
+                print(f"You chose the wrong turtle. The correct turtle was {tut.pencolor()}")
+                break
 
 screen.exitonclick()

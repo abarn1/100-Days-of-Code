b33461f69386e2880f9c3ef55f9213e3033b964c
Alex Barnes <35738417+abarn1@users.noreply.github.com>
2022-11-19 21:29:53 -0800

    Completed the Day 26 challenges

--- Day26/list_comprehension.py
+++ Day26/list_comprehension.py
@@ -15,3 +15,43 @@ print(f"Unsquared = {numbers}\nSquared = {squared_numbers}")
 # Filtering Even numbers
 filtered_numbers = [n for n in numbers if n % 2 == 0]
 print(f"Unfiltered = {numbers}\nFiltered = {filtered_numbers}")
+
+# Exercise for data overlap
+with open('file1.txt') as f:
+    file1 = [int(item.replace('\n', '')) for item in f.readlines()]
+with open('file2.txt') as f:
+    file2 = [int(item.replace('\n', '')) for item in f.readlines()]
+combined = [item for item in file1 if item in file2]
+print(combined)
+
+# Dictionary Comprehension
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+word_dictionary = {word:len(word) for word in sentence.split(' ')}
+print(word_dictionary)
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+weather_f = {day:(temp_c * 9/5) + 32 for (day, temp_c) in weather_c.items()}
+print(weather_f)
+
+# working with pandas
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+import pandas as pd
+student_data_frame = pd.DataFrame(student_dict)
+# loop through each of the rows in the pandas dataframe
+for (index, row) in student_data_frame.iterrows():
+    if row.student == "Angela":
+        print(row.score)
+
+
